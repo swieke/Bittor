5a539b76e267089a0184adf4dbcd5c239790f96c
Theo Azriel <theoazrie@gmail.com>
2020-01-27 14:11:53 +0800

    Initial Commit

---
+++ Bittor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bittor
+//
+//  Created by Theo Azriel on 27/01/20.
+//  Copyright © 2020 Azriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
